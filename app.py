a867589bd80cbc4a1b1dbd7ad35fdc35218c8ca2
huangluming <985285927@qq.com>
2022-08-03 16:57:50 +0800

    Add static files

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
+
 
 name = 'Grey Li'
 movies = [
